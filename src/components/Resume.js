3d60a4ea1b74841f0444063b1f8a1fc579b4392b
cedric <cedric.maigrot@ose-engineering.fr>
2023-01-14 12:54:44 +0100

    fix: change skills

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -74,50 +74,100 @@ export default class Resume extends Component {
 
           <div className="nine columns main-col">
 
-            <p>
-              {resumeData.skillsDescriptionLanguages}
-            </p>
-
             <div className="bars">
 
-              <ul className="skills">
-                {
-                  resumeData.skillsLanguages && resumeData.skillsLanguages.map((item) => {
-                    return (
-                      <li>
-                        <span className={`bar-expand ${item.skillname.toLowerCase()}`}>
-                        </span><em>{item.skillname}</em>
-                      </li>
-                    )
-                  })
-                }
+              <div className="skills">
+                <div className='badge react'>
+                  <img alt="react" src="/images/logo/react.png" />
+                  <span>React</span>
+                </div>
 
-              </ul>
+                <div className='badge python'>
+                  <img alt="python" src="/images/logo/python.png" />
+                  <span>Python</span>
+                </div>
 
+                <div className='badge html5'>
+                  <img alt="html5" src="/images/logo/html5.png" />
+                  <span>HTML5</span>
+                </div>
+
+                <div className='badge css3'>
+                  <img alt="css3" src="/images/logo/css3.png" />
+                  <span>CSS3</span>
+                </div>
+
+                <div className='badge java'>
+                  <img alt="java" src="/images/logo/java.png" />
+                  <span>JAVA</span>
+                </div>
+
+                <div className='badge php'>
+                  <img alt="php" src="/images/logo/php.png" />
+                  <span>PHP</span>
+                </div>
+
+                <div className='badge shell'>
+                  <img alt="shell" src="/images/logo/shell.png" />
+                  <span>Shell</span>
+                </div>
+
+                <div className='badge mysql'>
+                  <img alt="mysql" src="/images/logo/mysql.png" />
+                  <span>mySQL</span>
+                </div>
+
+                <div className='badge mongodb'>
+                  <img alt="mongodb" src="/images/logo/mongodb.png" />
+                  <span>mongoDB</span>
+                </div>
+
+                <div className='badge git'>
+                  <img alt="git" src="/images/logo/git.png" />
+                  <span>Git</span>
+                </div>
+
+                <div className='badge tableau'>
+                  <img alt="tableau" src="/images/logo/tableau.png" />
+                  <span>Tableau</span>
+                </div>
+
+              </div>
+
+              <div className="skills">
+
+                <div className='badge'>
+                  <img alt="ia" src="/images/logo/ia.png" />
+                  <span>Apprentissage automatique</span>
+                </div>
+
+                <div className='badge'>
+                  <img alt="ia" src="/images/logo/data.png" />
+                  <span>Analyse des données</span>
+                </div>
+
+                <div className='badge'>
+                  <img alt="ia" src="/images/logo/taln.png" />
+                  <span>Traitement Automatique des Langues</span>
+                </div>
+
+                <div className='badge'>
+                  <img alt="ia" src="/images/logo/image.png" />
+                  <span>Vision par ordinateur</span>
+                </div>
+
+                <div className='badge'>
+                  <img alt="ia" src="/images/logo/project.png" />
+                  <span>Gestion de projet</span>
+                </div>
+
+                <div className='badge'>
+                  <img alt="ia" src="/images/logo/agile.png" />
+                  <span>Méthodes agiles</span>
+                </div>
+
+              </div>
             </div>
-
-            <p>
-              {resumeData.skillsDescription}
-            </p>
-
-            <div className="bars">
-
-              <ul className="skills">
-                {
-                  resumeData.skills && resumeData.skills.map((item) => {
-                    return (
-                      <li>
-                        <span className={`bar-expand ${item.slug}`}>
-                        </span><em>{item.skillname}</em>
-                      </li>
-                    )
-                  })
-                }
-
-              </ul>
-
-            </div>
-
           </div>
 
         </div>
