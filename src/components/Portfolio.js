7994349342100957868b4c3058f18ed8947ef0b9
cedric <cedric.maigrot@ose-engineering.fr>
2023-01-14 12:55:12 +0100

    feat: logos.css

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,25 +10,38 @@ export default class Porfolio extends Component {
           <div className="twelve columns collapsed">
             <h1>Check Out Some of My Works.</h1>
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
-              {
-                resumeData.portfolio && resumeData.portfolio.map((item) => {
-                  return (
-                    <div className="columns portfolio-item">
-                      <div className="item-wrap">
-                        <a href="#modal-01">
-                          <img src="%PUBLIC_URL%/images/portfolio/coffee.jpg" className="item-img" />
-                          <div className="overlay">
-                            <div className="portfolio-item-meta">
-                              <h5>{item.name}</h5>
-                              <p>{item.description}</p>
-                            </div>
-                          </div>
-                        </a>
-                      </div>
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-01">
+                    {/* <img src="images/portfolio/coffee.jpg" className="item-img" /> */}
+                    <div className="portfolio-item-meta">
+                      <h5>Prévention des risques de suicide chez les adolescents cyberharcelés</h5>
                     </div>
-                  )
-                })
-              }
+                  </a>
+                </div>
+              </div>
+
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-01">
+                    {/* <img src="images/portfolio/coffee.jpg" className="item-img" /> */}
+                    <div className="portfolio-item-meta">
+                      <h5>Détection de fausses informations sur les réseaux sociaux</h5>
+                    </div>
+                  </a>
+                </div>
+              </div>
+
+              <div className="columns portfolio-item">
+                <div className="item-wrap">
+                  <a href="#modal-01">
+                    {/* <img src="images/portfolio/coffee.jpg" className="item-img" /> */}
+                    <div className="portfolio-item-meta">
+                      <h5>Moteur de recherche pour enfants</h5>
+                    </div>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
